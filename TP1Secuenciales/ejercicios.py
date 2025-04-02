971333aafa1bb0d72d5a9c1078bf7e6fe8a814ab
Luchiolm76 <lucianoolmedofrancisco@gmail.com>
2025-04-01 23:43:52 -0300

    Tp1-Secuenciales

---
+++ TP1Secuenciales/ejercicios.py
@@ -0,0 +1,62 @@
+#Ejercicio1
+#print("Hola Mundo!")
+
+#Ejercicio2
+#nombre = input ("Ingrese su nombre:")   
+#print (f"Hola {nombre}!")
+
+#Ejercicio3
+#nombre = input("Ingrese su nombre: ")
+#apellido = input("Ingrese su apellido: ")
+#edad = input("Ingrese su edad: ")
+#lugar_residencia = input("Ingrese su lugar de residencia: ")
+#print(f"Hola soy {nombre} {apellido}, tengo {edad} años y vivo en {lugar_residencia}")
+
+#Ejercicio4
+#radio = float(input( "Ingrese el radio: "))
+#pi = 3.14
+#print(f"El area del cirulo es:{pi*radio**2} ")
+#print(f"El perimetro del circulo es:{(radio*2)*pi}")
+
+#Ejercicio5
+#segundos = int(input("Ingrese la cantidad de segunods"))
+#horas = segundos / 3600
+#print(f"La cantidad de segundos ingresadas equivale a {horas} hs")
+
+#Ejercicio6
+#numero = int(input("Ingrese un número: "))
+#print(f"Tabla de multiplicar del numero {numero}.")
+#print(f"{numero} x 1 = {numero*1}")
+#print(f"{numero} x 2 = {numero*2}")
+#print(f"{numero} x 3 = {numero*3}")
+#print(f"{numero} x 4 = {numero*4}")
+#print(f"{numero} x 5 = {numero*5}")
+#print(f"{numero} x 6 = {numero*6}")
+#print(f"{numero} x 7 = {numero*7}")
+#print(f"{numero} x 8 = {numero*8}")
+#print(f"{numero} x 9 = {numero*9}")
+#print(f"{numero} x 10 = {numero*10}")
+
+#Ejercicio7
+#numero1 = int(input("Ingrese un numero entero distinto a 0:"))
+#numero2 = int(input("Ingrese un numero entero distinto a 0:"))
+#print (f"La suma de estos es:{numero1 + numero2}")
+#print (f"La resta de estos es:{numero1 - numero2}")
+#print (f"La multiplicacion de estos es: {numero1*numero2}")
+#print (f"La division de estos es: {numero1/numero2}")
+
+#Ejercicio8
+#altura = float(input("Ingrese su altura:"))
+#peso = float(input("Ingrese su peso:"))
+#print(f"El indice de masa corporal que usted tiene es de:{peso/altura**2}")
+
+#Ejercicio9
+#gradosC = float(input("Ingrese los grados celcius que hacen: "))
+#print (f"Su equivalente a fahrenheit es {9/5*gradosC+32}")
+
+#Ejercicio10
+#numero1 = float(input("Ingrese un numero entero:"))
+#numero2 = float(input("Ingrese un numero entero:"))
+#numero3 = float(input("Ingrese un numero entero:"))
+#promedio = (numero1 + numero2 + numero3) / 3
+#print(f"El promedio de los 3 numeros es:{promedio}")
