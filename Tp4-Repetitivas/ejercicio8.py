d086ff69d584ba28bfc248b24c29a8c4e206b314
Luchiolm76 <lucianoolmedofrancisco@gmail.com>
2025-04-21 10:03:28 -0300

    comiteo un arreglito

--- Tp4-Repetitivas/ejercicio8.py
+++ Tp4-Repetitivas/ejercicio8.py
@@ -3,7 +3,7 @@ impares = 0
 positivos = 0
 negativos = 0
 
-for i in range(5):
+for i in range(100):
     num = int(input("Ingrese 100 numeros enteros: "))
     if num % 2 == 0:
         pares = pares + 1
