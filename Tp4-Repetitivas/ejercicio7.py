1657f1dbd0bfd8a8bfbef7606717917a483357c3
Luchiolm76 <lucianoolmedofrancisco@gmail.com>
2025-04-21 09:56:59 -0300

    ejercicio8

---
+++ Tp4-Repetitivas/ejercicio7.py
@@ -0,0 +1,9 @@
+num = int(input("Ingresa un numero entero positivo: "))
+suma = 0
+i = 0
+
+while i <= num:
+    suma = suma + i
+    i = i + 1
+print(suma)
+    
---
+++ Tp4-Repetitivas/ejercicio8.py
@@ -0,0 +1,19 @@
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+
+for i in range(5):
+    num = int(input("Ingrese 100 numeros enteros: "))
+    if num % 2 == 0:
+        pares = pares + 1
+    else:
+        impares = impares + 1
+    if num > 0:
+        positivos = positivos + 1
+    else:
+        negativos = negativos + 1
+print(f"Cantidad de numeros pares:",pares)
+print(f"Cantidad de numeros impares:",impares)
+print(f"Cantidad de numeros positivos:",positivos)
+print(f"Cantidad de numeros negativos:",negativos)
