659627047bc2b7ac32cdecf860a37a8efd16cc13
Luchiolm76 <lucianoolmedofrancisco@gmail.com>
2025-06-07 07:55:54 -0300

    ejeercicio3

---
+++ TP6Funciones/ejercicio3.py
@@ -0,0 +1,9 @@
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+# Programa principal
+nombre = input("Nombre: ")
+apellido = input("Apellido: ")
+edad = input("Edad: ")
+residencia = input("Lugar de residencia: ")
+informacion_personal(nombre, apellido, edad, residencia)
