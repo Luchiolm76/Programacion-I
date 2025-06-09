ad29bef473a0934c44fa3223095a1b5962fd39f9
SNHillar <saulhillar@outlook.es>
2025-06-09 18:31:47 -0300

    agregando detalles para usuario

--- ProyectoIntegrador/proyectojuegos/menu.py
+++ ProyectoIntegrador/proyectojuegos/menu.py
@@ -3,39 +3,41 @@ from listajuegos import juegos
 
 
 
-
+#Menu principal del programa de juegos
 def menu():
 
-    juegos_ordenados = juegos[:]
+    juegos_ordenados = juegos[:] #copia de la lista original para no modificarla
     
     print("Bienvenido al programa de juegos")
     print("1. Mostrar lista de juegos")
-    print("2. Hacer ordenamiento de juegos por burbuja")
+    print("2. Hacer ordenamiento de juegos por burbuja")                ####OPCIONES DEL MENU
     print("3. Hacer ordenamiento de juegos rapido")
     print("4. Buscar un juego con busqueda binaria") 
     print("5. Buscar un juego con busqueda lineal") 
     print("6. Salir") 
     
-    
+    #entramos al bucle del menu
     while True:
         opcion = input("Seleccione una opción (1-6): ")
         if opcion == '1':
-            for juego in juegos_ordenados:
+            for juego in juegos_ordenados: #recorremos la lista de juegos y mostramos sus detalles
                 print(f"Título: {juego['titulo']}, Año: {juego['anio']}, Puntaje: {juego['puntaje']}")
         elif opcion == '2':
             key = input("¿Por qué clave desea ordenar? titulo/anio/puntaje: ")
-            juegos_ordenados = bubble_sort(juegos_ordenados, key)
+            juegos_ordenados = bubble_sort(juegos_ordenados, key) #llamamos a la funcion de ordenamiento por burbuja
+            #mostramos los juegos ordenados
             print("Lista ordenada por burbuja.")
-            for juego in juegos_ordenados:
+            for juego in juegos_ordenados: #recorremos la lista de juegos y mostramos sus detalles
                 print(f"Título: {juego['titulo']}, Año: {juego['anio']}, Puntaje: {juego['puntaje']}")
         elif opcion == '3':
-            key = input("¿Por qué clave desea ordenar? titulo/anio/puntaje: ")
-            juegos_ordenados = quick_sort(juegos_ordenados, key)
+            key = input("¿Por qué clave desea ordenar? titulo/anio/puntaje: ") #aca medimos la clave por la que se ordenara
+            juegos_ordenados = quick_sort(juegos_ordenados, key) #llamamos a la funcion de ordenamiento rapido y le pasamos la clave
+            #mostramos los juegos ordenados
             print("Lista ordenada por quick sort.")
-            for juego in juegos_ordenados:
+            for juego in juegos_ordenados: #recorremos la lista de juegos y mostramos sus detalles
                 print(f"Título: {juego['titulo']}, Año: {juego['anio']}, Puntaje: {juego['puntaje']}")
         elif opcion == '4':
-            clave = input("Ingrese la clave por la que desea buscar (titulo, anio, puntaje): ")
+            clave = input("Ingrese la clave por la que desea buscar (titulo, anio, puntaje): ") #aca de nuevo pedimos la clave por la que se buscara
             valor = input(f"Ingrese el valor de {clave} que desea buscar: ")
             if clave in ['titulo', 'anio', 'puntaje']:
                 if clave == 'anio':
@@ -53,6 +55,8 @@ def menu():
                         print(f"Título: {juego['titulo']}, Año: {juego['anio']}, Puntaje: {juego['puntaje']}")
                 else:
                     print("No se encontraron resultados.")
+            else:
+                print("Clave no válida. Por favor, elija entre 'titulo', 'anio' o 'puntaje'.")
         elif opcion == '5':
             clave = input("Ingrese la clave por la que desea buscar (titulo, anio, puntaje): ")
             valor = input(f"Ingrese el valor de {clave} que desea buscar: ")
@@ -66,6 +70,8 @@ def menu():
                         print(f"Título: {juego['titulo']}, Año: {juego['anio']}, Puntaje: {juego['puntaje']}")
                 else:
                     print("No se encontraron resultados.")
+            else:
+                print("Clave no válida. Por favor, elija entre 'titulo', 'anio' o 'puntaje'.")
         elif opcion == '6':
             print("Saliendo del programa. ¡Hasta luego!")
             break
